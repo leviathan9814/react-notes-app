e4905c501ff70ca0575d4fb469c2a3cc5b691d3e
Yurii Tsiupa <zver.enigma98@gmail.com>
2021-08-23 15:43:26 +0300

    update app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+
+export const Navbar = () => (
+    <nav className="navbar navbar-dark navbar-expand-lg bg-primary">
+        <div className="navbar-brand">
+            Note App
+        </div>
+
+        <ul className="navbar-nav">
+            <li className="nav-item">
+                <NavLink to="/" exact className="nav-link" >
+                    Home
+                </NavLink>
+            </li>
+            <li className="nav-item">
+                <NavLink to="/about" exact className="nav-link" >
+                    About
+                </NavLink>
+            </li>
+        </ul>
+    </nav>
+)
